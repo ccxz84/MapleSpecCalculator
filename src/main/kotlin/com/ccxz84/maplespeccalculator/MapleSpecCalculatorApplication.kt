80ae9d946cd428444965de5f334996cc11bf93c6
ccxz84 <ccxz84@naver.com>
2023-07-31 04:18:14 +0900

    initial commit

---
+++ src/main/kotlin/com/ccxz84/maplespeccalculator/MapleSpecCalculatorApplication.kt
@@ -0,0 +1,11 @@
+package com.ccxz84.maplespeccalculator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MapleSpecCalculatorApplication
+
+fun main(args: Array<String>) {
+    runApplication<MapleSpecCalculatorApplication>(*args)
+}
