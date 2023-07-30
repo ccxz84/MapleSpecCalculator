1c18355fa149f8b722824356eacdd2917c7f797d
ccxz84 <ccxz84@naver.com>
2023-07-31 03:49:53 +0900

    initial commit

---
+++ src/test/kotlin/com/ccxz84/maplespeccalculator/MapleSpecCalculatorApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ccxz84.maplespeccalculator
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MapleSpecCalculatorApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
